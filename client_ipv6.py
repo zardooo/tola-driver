714a98aac9de622df5e8422bd6bf6e703dc3e45e
zardooo <50323333+zardooo@users.noreply.github.com>
2019-05-17 00:26:54 +0800

    Add files via upload

---
+++ client_ipv6.py
@@ -0,0 +1,33 @@
+import socket
+import time
+
+mysock = socket.socket(socket.AF_INET6,socket.SOCK_STREAM)
+#mysock.connect(("2001:470:4c22:ae86:babe:41:10b9:6b2e",65420))
+#mysock.connect(("2402:f000:4:33:808::8ce",65420))
+mysock.connect(("",65420))
+
+mysock.send(b'hello')
+time.sleep(1)
+
+msg = mysock.recv(1024)
+print(msg.decode())
+
+mysock.send(b'run')
+time.sleep(1)
+
+#input start time
+print('input start time [Y-M-D H:M:S][2000-01-01 01:01:01]:')
+#print('for example: 2019-05-9 8:36:10')
+datetime = input()
+mysock.send(datetime.encode())
+
+#input centre frequency
+print('input centre frequency(MHz):')
+freq = float(input()) * 1000000
+mysock.send(str(freq).encode())
+
+#mysock.send(b'shutdown')
+time.sleep(1)
+mysock.send((str(90*1000*1000)).encode())
+
+time.sleep(600)
